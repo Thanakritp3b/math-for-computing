98d38fe974a67cf2fceca1cef6a3d3cc9aafca2c
Thanakrit Pongtanawannagon <thanakrit03b@gmail.com>
2025-02-13 22:53:08 +0700

    done

--- Cw_polynomial/polynomial.py
+++ Cw_polynomial/polynomial.py
@@ -1,15 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def f(x):
-    return np.sin(x)
+    # return np.sin(x) 
+    return np.sin(3*x)
 
 def gen_x(a, b, n):
-    x = []
-    for i in range(n):
-        x.append(a + (b - a) * i / (n - 1))
-    return x
+    return np.linspace(a, b, n)  
 
 def get_matrix(x):
     matrix = []
@@ -27,7 +24,6 @@ def evaluate_polynomial(coefficients, x_values):
         y_values += coefficients[i] * np.power(x_values, i)
     return y_values
 
-
 def lagrange_polynomial(x, y, x_values):
     n = len(x)
     y_values = np.zeros_like(x_values)
@@ -43,30 +39,24 @@ def plot_polynomial(x, y, coefficients, x_values, y_real):
     y_values = evaluate_polynomial(coefficients, x_values)
     plt.scatter(x, y, label='Data Points')
     plt.plot(x_values, y_values, label='Polynomial Interpolation')
-    plt.plot(x_values, y_real, color='red', label='Interpolated Points')
+    plt.plot(x_values, y_real, color='red', label='True Function')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
     plt.title('Polynomial Interpolation')
     plt.show()
 
-
-matrix_size = 9
-num_points = 14
-x = gen_x(-matrix_size, matrix_size, num_points)
+x = gen_x(-10, 10, 7)
 y = [f(xi) for xi in x]
 matrix = get_matrix(x)
-# print(matrix)
 solve_matrix = np.linalg.solve(matrix, y)
-print(solve_matrix)
+
 interp_x = np.linspace(-10, 10, 100)
 real_y = [f(xi) for xi in interp_x]
+
 plot_polynomial(x, y, solve_matrix, interp_x, real_y)
 
-
-coefficients = lagrange_polynomial(x, y, interp_x)
-print(coefficients)
-plt.figure()  
+plt.figure()
 lagrange_y = lagrange_polynomial(x, y, interp_x)
 plt.scatter(x, y, label='Data Points')
 plt.plot(interp_x, lagrange_y, label='Lagrange Interpolation')
@@ -76,9 +66,3 @@ plt.ylabel('y')
 plt.legend()
 plt.title('Lagrange Interpolation')
 plt.show()
-
-
-
-
-
-
